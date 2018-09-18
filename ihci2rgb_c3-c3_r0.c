ab6bf85f7192e0884a6831157fd4f895d52f52e4
Flora Canou <floracanou@qq.com>
2018-09-18 22:49:21 +0800

    Version C3-C3 (1.1.0) (#3)

--- ihci2rgb_c3-c3_r0.c
+++ ihci2rgb_c3-c3_r0.c
@@ -1,7 +1,7 @@
-/* Copyright 2018 Flora Canou, Alexander Zheng | V. C2-C2 (1.0.3) | Improved HCI to RGB Convertor
+/* Copyright 2018 Flora Canou, Alexander Zheng | V. C3-C3 (1.1.0) | Improved HCI to RGB Convertor
  * This Source Code Form is licensed under the Mozilla Public License, v. 2.0. 
  * If a copy of the MPL was not distributed with this file, you can obtain one at https://mozilla.org/MPL/2.0/. 
- * The program converts an entry in an Improved HCI color space to the corresponding RGB representation. 
+ * The program converts an entry in an Improved HCI color model to the corresponding RGB representation. 
  * See Improved HCI Color Model Specifications for more info. 
  */
 
@@ -9,13 +9,11 @@
 #include <math.h>
 #define pi	3.14159265
 
-void convertBack (double h_deg, double c, double i, double *r, double *g, double *b)
+void ihciBack (double h, double c, double i, double *p1, double *p2, double *p3)
 {
-	double h_rad; //hue in radians
-	h_rad = h_deg * pi / 180;
-	*r = i + 2 * c * cos (h_rad) / 3;
-	*g = i + 2 * c * cos (h_rad - 2 * pi / 3) / 3;
-	*b = i + 2 * c * cos (h_rad - 4 * pi / 3) / 3;
+	*p1 = i + 2 * c * cos (h) / 3;
+	*p2 = i + 2 * c * cos (h - 2 * pi / 3) / 3;
+	*p3 = i + 2 * c * cos (h - 4 * pi / 3) / 3;
 }
 
 void instruct (void);
@@ -31,18 +29,18 @@ int main (void)
 		scanf ("%lf %lf %lf", &h, &c, &i);
 		if (h == -1 && c == -1 && i == -1)
 			return 0;
-		
-		convertBack (h, c, i, &r, &g, &b);
-		printf ("Red\t= %f, \nGreen\t= %f, \nBlue\t= %f. \n", r, g, b);
-		
-		if (h < 0 || c < 0 || i < 0 || h > 360 || c > 1 || i > 1)
+		else if (h < 0 || c < 0 || i < 0 || h > 360 || c > 1 || i > 1)
 			printf ("Warning: One or more values are out of range. The result may be erroneous. \n");
+		
+		h = h * pi / 180; //degree to radian
+		ihciBack (h, c, i, &r, &g, &b);
+		printf ("Red\t= %f, \nGreen\t= %f, \nBlue\t= %f. \n", r, g, b);
 	}
 }
 
 void instruct (void)
 {
-	printf ("This program converts an entry in an Improved HCI color space to the corresponding RGB representation. \n"
+	printf ("This program converts an entry in an Improved HCI color model to the corresponding RGB representation. \n"
 			"You will be asked to enter the values for hue, chroma and intensity, respectively. \n"
 			"The hue should be in degrees, between 0 and 360. The other two should be between 0 and 1. \n"
 			"Note: In case the hue is undefined, enter any value for hue and 0 for chroma. \n"
